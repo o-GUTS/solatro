c0205817ea4f886ad6f4bc8b51f2e4c0bc794baa
Gustavo dos santos <gustavocomercialcontact@gmail.com>
2026-04-29 09:44:44 -0300

    added music
    Added background music for menu, game and shop and a simple music transition tween

--- src/screens/options/options_screen.gd
+++ src/screens/options/options_screen.gd
@@ -3,13 +3,19 @@ extends Control
 
 
 @onready var back_btn: SoundButton = %backBtn
-@onready var master_slider: HSlider = %masterSlider
+@onready var master_slider: HSlider = %master_slider
+@onready var music_slider: HSlider = %music_slider
+@onready var effects_slider: HSlider = %effects_slider
 
-@onready var bus_idx: int = AudioServer.get_bus_index("Master")
+@onready var master_bus_idx: int = AudioServer.get_bus_index(&"Master")
+@onready var music_bus_idx: int = AudioServer.get_bus_index(&"Music")
+@onready var effects_bus_idx: int = AudioServer.get_bus_index(&"Effects")
 
 
 func _ready() -> void:
-    master_slider.value = AudioServer.get_bus_volume_db(bus_idx)
+    master_slider.value = AudioServer.get_bus_volume_db(master_bus_idx)
+    music_slider.value = AudioServer.get_bus_volume_db(music_bus_idx)
+    effects_slider.value = AudioServer.get_bus_volume_db(effects_bus_idx)
 
 
 func _on_back_btn_pressed() -> void:
@@ -18,4 +24,12 @@ func _on_back_btn_pressed() -> void:
 
 
 func _on_master_slider_value_changed(value: float) -> void:
-    AudioServer.set_bus_volume_db(bus_idx, value)
+    AudioServer.set_bus_volume_db(master_bus_idx, value)
+
+
+func _on_music_slider_value_changed(value: float) -> void:
+    AudioServer.set_bus_volume_db(music_bus_idx, value)
+
+
+func _on_effects_slider_value_changed(value: float) -> void:
+    AudioServer.set_bus_volume_db(effects_bus_idx, value)
--- src/transition_manager/screen_transition_manager.gd
+++ src/transition_manager/screen_transition_manager.gd
@@ -6,15 +6,26 @@ enum Screens {
     title, options, credits, game, shop
 }
 
+enum Musics {
+    menu, game, shop
+}
+
 const title_screen_path: String = "res://src/screens/title/title_screen.tscn"
 const options_screen_path: String = "res://src/screens/options/options_screen.tscn"
 const credits_screen_path: String = "res://src/screens/credits/credits_screen.tscn"
 const game_screen_path: String = "res://src/screens/game/game_screen.tscn"
 const shop_screen_path: String = "res://src/screens/shop/shop_screen.tscn"
 
+const menu_music_path: String = "res://assets/musics/menu_music.ogg"
+const game_music_path: String = "res://assets/musics/game_music.ogg"
+const shop_music_path: String = "res://assets/musics/shop_music.ogg"
+
+var current_music := Musics.menu
+
 @onready var fade_rect: ColorRect = %fade_rect
 @onready var screen_parent: Node = %screen_parent
 @onready var anim: AnimationPlayer = %anim
+@onready var bg_music: AudioStreamPlayer = %bg_music
 
 
 func transtition_to(screen: Screens) -> void:
@@ -28,6 +39,7 @@ func transtition_to(screen: Screens) -> void:
         Screens.title:
             var screen_scene: PackedScene = load(title_screen_path)
             screen_parent.add_child(screen_scene.instantiate())
+            transition_music(Musics.menu)
 
         Screens.options:
             var screen_scene: PackedScene = load(options_screen_path)
@@ -40,12 +52,37 @@ func transtition_to(screen: Screens) -> void:
         Screens.game:
             var screen_scene: PackedScene = load(game_screen_path)
             screen_parent.add_child(screen_scene.instantiate())
+            transition_music(Musics.game)
 
         Screens.shop:
             var screen_scene: PackedScene = load(shop_screen_path)
             screen_parent.add_child(screen_scene.instantiate())
+            transition_music(Musics.shop)
 
     anim.play(&"fade_out")
     await anim.animation_finished
 
     anim.play(&"RESET")
+
+
+func transition_music(music: Musics) -> void:
+    if current_music == music: return
+
+    var volume_tween: Tween = create_tween()
+    volume_tween.tween_property(bg_music, ^"volume_db", -50, 0.5)
+    volume_tween.tween_property(bg_music, ^"volume_db", -5, 0.5)
+
+    await volume_tween.step_finished
+
+    bg_music.stop()
+
+    match music:
+        Musics.menu:
+            bg_music.stream = load(menu_music_path)
+        Musics.game:
+            bg_music.stream = load(game_music_path)
+        Musics.shop:
+            bg_music.stream = load(shop_music_path)
+
+    bg_music.play()
+    current_music = music
