e4e0873752e857729ac0cd925908e0cfd785121c
Gustavo dos santos <gustavocomercialcontact@gmail.com>
2026-04-29 08:31:15 -0300

    added screen fade in/out

--- src/transition_manager/screen_transition_manager.gd
+++ src/transition_manager/screen_transition_manager.gd
@@ -12,32 +12,40 @@ const credits_screen_path: String = "res://src/screens/credits/credits_screen.ts
 const game_screen_path: String = "res://src/screens/game/game_screen.tscn"
 const shop_screen_path: String = "res://src/screens/shop/shop_screen.tscn"
 
-
-func _ready() -> void:
-    transtition_to(Screens.title)
+@onready var fade_rect: ColorRect = %fade_rect
+@onready var screen_parent: Node = %screen_parent
+@onready var anim: AnimationPlayer = %anim
 
 
 func transtition_to(screen: Screens) -> void:
-    for node in get_children():
+    anim.play(&"fade_in")
+    await anim.animation_finished
+
+    for node in screen_parent.get_children():
         node.queue_free()
 
     match screen:
         Screens.title:
             var screen_scene: PackedScene = load(title_screen_path)
-            add_child(screen_scene.instantiate())
+            screen_parent.add_child(screen_scene.instantiate())
 
         Screens.options:
             var screen_scene: PackedScene = load(options_screen_path)
-            add_child(screen_scene.instantiate())
+            screen_parent.add_child(screen_scene.instantiate())
 
         Screens.credits:
             var screen_scene: PackedScene = load(credits_screen_path)
-            add_child(screen_scene.instantiate())
+            screen_parent.add_child(screen_scene.instantiate())
 
         Screens.game:
             var screen_scene: PackedScene = load(game_screen_path)
-            add_child(screen_scene.instantiate())
+            screen_parent.add_child(screen_scene.instantiate())
 
         Screens.shop:
             var screen_scene: PackedScene = load(shop_screen_path)
-            add_child(screen_scene.instantiate())
+            screen_parent.add_child(screen_scene.instantiate())
+
+    anim.play(&"fade_out")
+    await anim.animation_finished
+
+    anim.play(&"RESET")
